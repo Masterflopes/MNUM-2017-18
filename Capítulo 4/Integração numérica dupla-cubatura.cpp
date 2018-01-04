570ba08d130706a68b6d30b65b470e9c837bf148
Unknown <nunolopes98@hotmail.com>
2018-01-04 17:26:21 +0000

    Updating chapter 4

---
+++ Capítulo 4/Integração numérica dupla-cubatura.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+//Integrac‹o numerica dupla ou cubatura
+
+double f(double x) {
+	return sin(x) / pow(x, 2);
+}
+
+
+double f(double x, double y) {
+	return exp(y - x);
+}
+
+int main() {
+	double nx = 2, ny = 2;
+	double a = 0, b = 0.5;
+	double hx = (b - a) / nx, hy = (b - a) / ny;
+
+	double soma0 = f(0, 0) + f(0, 0.5) + f(0.5, 0) + f(0.5, 0.5);
+	double soma1 = f(0, 0.25) + f(0.25, 0) + f(0.5, 0.25) + f(0.25, 0.5);
+	double soma2 = f(0.25, 0.25);
+
+	double st = hx*hy / 4 * (soma0 + 2 * soma1 + 4 * soma2);
+	double ss = hx*hy / 9 * (soma0 + 4 * soma1 + 16 * soma2);
+
+	cout << "st: " << st << endl;
+	cout << "ss: " << ss << endl;
+
+	system("pause");
+}
